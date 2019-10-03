a36cfe26f21256d5eec714f4b55c03811b20bbe8
25 macamp <rateliasudaisuki@gmail.com>
2019-10-03 23:52:05 +0900

    PropTypes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, actions }) => {
@@ -9,6 +10,17 @@ const TodoList = ({ todos, actions }) => {
       ))}
     </ul>
   );
+};
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired
+    }).isRequired
+  ).isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 export default TodoList;
